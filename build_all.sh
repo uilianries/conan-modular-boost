7ddcb4a3c8c739a1c71b98bb2abf8622683d07b0
Uilian Ries <uilianries@gmail.com>
2025-03-22 10:24:12 +0100

    Fix Boost hof download
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build_all.sh
+++ build_all.sh
@@ -4,12 +4,12 @@ set -ex
 
 BOOST_VERSION=1.87.0
 
-for folder in $(find . -depth 1 -type d -name "boost-*"); do
+for folder in $(find . -maxdepth 1 -type d -name "boost-*"); do
     echo "Exporting $folder"
     conan export $folder/all --version=$BOOST_VERSION
 done
 
-for folder in $(find . -depth 1 -type d -name "boost-*"); do
+for folder in $(find . -maxdepth 1 -type d -name "boost-*"); do
     echo "Exporting $folder"
     conan create $folder/all --version=$BOOST_VERSION --build=missing
 done
