c81029aac3ea68597865d0e36e6a4f27c65426b2
Uilian Ries <uilianries@gmail.com>
2025-03-22 12:55:14 +0100

    Use Build order
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import argparse
+import json
 
 
 if __name__ == '__main__':
@@ -14,8 +15,22 @@ if __name__ == '__main__':
             print(f'Exporting {folder}')
             subprocess.run(f'conan export {folder}/all --version={boost_version}', shell=True)
     
+    temp_dir = os.getenv('RUNNER_TEMP') or "/tmp"
+    conanfile = f'{temp_dir}/conanfile.txt'
+    boost_modules = []
     for folder in os.listdir('.'):
         if os.path.isdir(folder) and folder.startswith('boost-'):
-            print(f'Creating {folder}')
-            subprocess.run(f'conan create {folder}/all --version={boost_version} --build=missing', shell=True)
-    
+            boost_modules.append(folder)
+    with open(conanfile, 'w') as fd:
+        fd.write("[requires]\n")
+        for module in boost_modules:
+            fd.write(f"{module}/{boost_version}\n")
+
+    context = subprocess.run(f'conan graph build-order {conanfile} --format=json', shell=True, capture_output=True, text=True)
+    graph = json.loads(context.stdout)
+    for level in graph:
+        for node in level:
+            reference = node['ref'][:node['ref'].find('/')]
+            if reference.startswith('boost-') and os.path.isdir(reference):
+                print(f'Creating {reference}')
+                subprocess.run(f'conan create {reference}/all --version={boost_version} --build=missing', shell=True)
