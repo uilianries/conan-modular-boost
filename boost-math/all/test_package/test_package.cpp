43edd5816fe49ce10b96d3072ed6fd6b4e679b08
Uilian Ries <uilianries@gmail.com>
2025-03-23 19:48:08 +0100

    Add Boost Math
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

---
+++ boost-math/all/test_package/test_package.cpp
@@ -0,0 +1,7 @@
+#include <boost/math/special_functions/erf.hpp>
+#include <iostream>
+
+int main() {
+    std::cout << "Boost Math test package: " << boost::math::erf(1.0) << std::endl;
+    return 0;
+}
