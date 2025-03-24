8dd2f9b576c1ce6feb036116ae1fa81ac688846f
Uilian Ries <uilianries@gmail.com>
2025-03-24 07:59:55 +0100

    Fail in case of error in CI
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -2,9 +2,13 @@ import os
 import subprocess
 import argparse
 import json
+import logging
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('-bv', '--boost-version', type=str, default='1.87.0')
     args = arg_parser.parse_args()
@@ -12,8 +16,8 @@ if __name__ == '__main__':
 
     for folder in os.listdir('.'):
         if os.path.isdir(folder) and folder.startswith('boost-'):
-            print(f'Exporting {folder}')
-            subprocess.run(f'conan export {folder}/all --version={boost_version}', shell=True)
+            logger.info(f'Exporting {folder}')
+            subprocess.run(f'conan export {folder}/all --version={boost_version}', shell=True, check=True)
 
     temp_dir = os.getenv('RUNNER_TEMP') or "/tmp"
     conanfile = os.path.join(temp_dir, 'conanfile.txt')
@@ -26,7 +30,7 @@ if __name__ == '__main__':
         for module in boost_modules:
             fd.write(f"{module}/{boost_version}\n")
 
-    context = subprocess.run(f'conan graph build-order {conanfile} --format=json', shell=True, capture_output=True, text=True)
+    context = subprocess.run(f'conan graph build-order {conanfile} --format=json', shell=True, capture_output=True, text=True, check=True)
     graph = json.loads(context.stdout)
     references = []
     for level in graph:
@@ -35,10 +39,8 @@ if __name__ == '__main__':
             if reference.startswith('boost-') and os.path.isdir(reference):
                 references.append(reference)
 
-    print("Build order:")
-    for reference in references:
-        print(reference)
+    logger.info(f"Build order: {references}")
 
     for reference in references:
-        print(f'Creating {reference}')
-        subprocess.run(f'conan create {reference}/all --version={boost_version} --build=missing -s compiler.cppstd=17', shell=True)
+        logger.info(f'Creating {reference}')
+        subprocess.run(f'conan create {reference}/all --version={boost_version} --build=missing -s compiler.cppstd=17', shell=True, check=True)
