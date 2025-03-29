820beb9ad25fddaa6de114d8acc0a6b4bb93d988
Uilian Ries <uilianries@gmail.com>
2025-03-29 09:22:47 +0100

    Add more options to build script
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -10,24 +10,45 @@ if __name__ == '__main__':
     logger = logging.getLogger(__name__)
 
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('-bv', '--boost-version', type=str, default='1.87.0')
+    arg_parser.add_argument('-bv', '--boost-version', type=str, default='1.87.0', description="Boost project version")
+    arg_parser.add_argument('-se', '--skip-export', action='store_true', description="Do not run conan export for all Boost modules")
+    arg_parser.add_argument('-sc', '--skip-create', action='store_true', description="Do not run conan create for all Boost modules")
     args = arg_parser.parse_args()
     boost_version = args.boost_version
+    folder_list = []
+    header_libraries = []
+    regular_libraries = []
 
     for folder in os.listdir('.'):
         if os.path.isdir(folder) and folder.startswith('boost-'):
+            folder_list.append(folder)
+
+    logger.info(f"Found {len(folder_list)} Boost modules in total to be exported.")
+
+    for folder in folder_list:
+        conanfile_path = os.path.join(folder, 'all', 'conanfile.py')
+        if os.path.exists(conanfile_path):
+            with open(conanfile_path, 'r') as conanfile:
+                content = conanfile.read()
+                if 'header-library' in content:
+                    header_libraries.append(folder)
+                else:
+                    regular_libraries.append(folder)
+    
+    logger.info(f"Found {len(header_libraries)} Boost modules that are header-only libraries.")
+    logger.info(f"Found {len(regular_libraries)} Boost modules that are regular libraries.")
+
+    if not args.skip_export:
+        for folder in folder_list:
             logger.info(f'Exporting {folder}')
             subprocess.run(f'conan export {folder}/all --version={boost_version}', shell=True, check=True)
 
     temp_dir = os.getenv('RUNNER_TEMP') or "/tmp"
     conanfile = os.path.join(temp_dir, 'conanfile.txt')
-    boost_modules = []
-    for folder in os.listdir('.'):
-        if os.path.isdir(folder) and folder.startswith('boost-'):
-            boost_modules.append(folder)
+
     with open(conanfile, 'w') as fd:
         fd.write("[requires]\n")
-        for module in boost_modules:
+        for module in folder_list:
             fd.write(f"{module}/{boost_version}\n")
 
     context = subprocess.run(f'conan graph build-order {conanfile} --format=json --build=missing -s compiler.cppstd=20', shell=True, capture_output=True, text=True, check=True)
@@ -41,6 +62,7 @@ if __name__ == '__main__':
 
     logger.info(f"Build order: {references}")
 
-    for reference in references:
-        logger.info(f'Creating {reference}')
-        subprocess.run(f'conan create {reference}/all --version={boost_version} --build=missing -s compiler.cppstd=20', shell=True, check=True)
+    if not args.skip_create:
+        for reference in references:
+            logger.info(f'Creating {reference}')
+            subprocess.run(f'conan create {reference}/all --version={boost_version} --build=missing -s compiler.cppstd=20', shell=True, check=True)
